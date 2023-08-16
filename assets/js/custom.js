b18c654b94e963765914d46fa68cbd1d77f70f49
swowst <staghi@bk.ru>
2023-08-16 18:21:14 +0400

    added qty and delete item buttons

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 var basket = document.querySelector(".basket")
 
-document.querySelector(".basket-container").addEventListener("click", function(){
+document.querySelector(".toggle-div").addEventListener("click", function(){
     if(basket.classList.contains("d-none")){
         basket.classList.remove("d-none")
         basket.classList.add("d-block")
@@ -9,3 +9,33 @@ document.querySelector(".basket-container").addEventListener("click", function()
         basket.classList.add("d-none")
     }
 })
+
+
+
+
+const decreaseButton = document.getElementById('decrease');
+    const increaseButton = document.getElementById('increase');
+    const countInput = document.getElementById('count');
+
+    decreaseButton.addEventListener('click', () => {
+      const currentValue = parseInt(countInput.value, 10);
+      if (currentValue > 0) {
+        countInput.value = currentValue - 1;
+      }
+    });
+
+    increaseButton.addEventListener('click', () => {
+      const currentValue = parseInt(countInput.value, 10);
+      if (currentValue < 999) {
+        countInput.value = currentValue + 1;
+      }
+    });
+
+
+
+    $(document).ready(function() {
+      $('.btn-close-item').click(function() {
+        $(this).closest('.card-item').remove();
+      });
+    });
+    
