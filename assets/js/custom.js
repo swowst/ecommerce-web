a632910637a1e49328f24d311b6edc1a0525d0bc
smst1yv <mstfvsexavet@gmail.com>
2023-08-17 17:19:02 +0400

    product foto slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -50,3 +50,14 @@ $(document).ready(function() {
       });
     });
     
+
+
+
+
+
+    var splide = new Splide( '.splide', {
+      perPage: 3,
+      rewind : true,
+    } );
+    
+    splide.mount();
